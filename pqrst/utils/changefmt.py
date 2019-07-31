6e61588a61c1d181119f6d68e3300a60f25db585
Shreya <shreya2feb@gmail.com>
2019-07-31 23:50:15 +0530

    fix: Numpy to csv dislocation error

--- pqrst/utils/changefmt.py
+++ pqrst/utils/changefmt.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from pqrst.utils.savitzky_golay import savitzky_golay
 
 
 
@@ -10,14 +11,16 @@ def npy_to_csv(path,scaler_factor=None,make_abs=False):
     df_array = []
     if scaler_factor != None:
         print("scaling...")
-        data_ = 1000*data
+        data_ = scaler_factor * data
         if make_abs == True:
             print("Absoluting...")
             for i in range(len(data_)):
-                df = pd.DataFrame(data_[i], index=None, columns=None, dtype=None)
-                min_ = np.amin(data_[i])
-                df_ = [x[0] - min_ for x in df.values]
-                df_array.append(df_)
+                entropy = savitzky_golay(data_[i], 411, 3)
+                df = pd.DataFrame(entropy)
+                # df.to_csv("data"+ i +"".csv",index=False)
+                # min_ = np.amin(data_[i])
+                # df_ = [x[0] - min_ for x in df.values]
+                df_array.append(df)
         else:
             for i in range(len(data_)):
                 df = pd.DataFrame(data_[i], index=None, columns=None, dtype=None)
@@ -36,5 +39,4 @@ def npy_to_csv(path,scaler_factor=None,make_abs=False):
                 df = pd.DataFrame(data[i], index=None, columns=None, dtype=None)
                 df_ = [x[0] for x in df.values]
                 df_array.append(df_)
-
     return df_array
--- tutorial.py
+++ tutorial.py
@@ -14,30 +14,31 @@ from scipy.signal import savgol_filter
 current_dir = pathlib.Path(__file__).resolve()
 
 example_dir_npy = current_dir.parent/'data'/'image5_signal.npy'
-example_dir_csv = current_dir.parent/'data'/'data.csv'
+example_dir_csv = current_dir.parent/'data'/'data_2.csv'
 
 data = npy_to_csv(example_dir_npy, scaler_factor = 1, make_abs = True)
 data_ = load_csv(example_dir_csv)
-for i in range(len(data)):
-    print(data[i][5])
 
 
-
-# print(yhat)
-
-fs = len(data[0])//3
+fs = 250
 
 detectors = Detectors(fs)
 
-# r_peaks = detectors.two_average_detector(data[3])
-# print(r_peaks)
+
+#
+# r_peaks_ = detectors.two_average_detector(data[2].values)
+# # print(data_)
+#
+# plt.plot(data[2].values)
+# plt.plot(r_peaks_, [data[2].values[j] for j in r_peaks_], 'ro')
+# plt.show()
 
 r_peaks_list = []
 for i in range(15):
     r_peaks = detectors.two_average_detector(data[i])
     r_peaks_list.append(r_peaks)
 
-plot_signal(data,r_peaks_list)
+# plot_signal(data,r_peaks_list)
 
 
 #first applying smoothening filter
@@ -45,9 +46,8 @@ plot_signal(data,r_peaks_list)
 smooth_data = []
 smooth_peaks = []
 for i in range(15):
-    yhat = savitzky_golay(data[i], 211, 3)
-    peaks = detectors.two_average_detector(yhat)
-    smooth_data.append(yhat)
+    peaks = detectors.two_average_detector(data[i].values)
+    smooth_data.append(data[i].values)
     smooth_peaks.append(peaks)
 
 
